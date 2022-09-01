280375349c2506486bd7785831eb19d3fba25b0c
凤之兮原 <1577310596@qq.com>
2022-09-01 21:30:29 +0800

    chore: add Config to Unocss

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetUno(), presetAttributify()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
