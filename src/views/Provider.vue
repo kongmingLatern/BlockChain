30ad466894f33ebe10088e0eb436c9f49c632f93
凤之兮原 <1577310596@qq.com>
2022-09-02 21:26:14 +0800

    chore: add  props to Provider

--- src/views/Provider.vue
+++ src/views/Provider.vue
@@ -1,36 +1,65 @@
 <template>
   <h1>Provider</h1>
-  <table ml-10 border w-100 text-center>
-    <th>商品名称</th>
-    <th>最低售价</th>
-    <th>购买</th>
-    <GoodsItmes :goods="goodsItem" />
-  </table>
-
+  <ProviderTable seller="Ming" :goodsItem="MinggoodsItem" />
+  <ProviderTable seller="Li" :goodsItem="LigoodsItem" />
+  <ProviderTable seller="Xiao" :goodsItem="XiaogoodsItem" />
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import ProviderTable from "@/components/ProviderTable.vue";
+import type { GoodsItem } from "@/type";
 import { reactive } from "vue";
-import GoodsItmes from "../components/GoodsItmes.vue";
-import type { GoodsItem } from '../type';
-const goodsItem = reactive<GoodsItem[]>([
+const MinggoodsItem = reactive<GoodsItem[]>([
   {
     id: 1,
     name: "apple",
-    price: '10BTB',
+    price: "10BTB",
   },
   {
     id: 2,
     name: "banana",
-    price: '10BTB',
+    price: "10BTB",
   },
   {
     id: 3,
     name: "orange",
-    price: '15BTB',
+    price: "15BTB",
+  },
+]);
+
+const LigoodsItem = reactive<GoodsItem[]>([
+  {
+    id: 1,
+    name: "apple",
+    price: "5BTB",
+  },
+  {
+    id: 2,
+    name: "banana",
+    price: "9BTB",
+  },
+  {
+    id: 3,
+    name: "orange",
+    price: "15BTB",
+  },
+]);
+
+const XiaogoodsItem = reactive<GoodsItem[]>([
+  {
+    id: 1,
+    name: "apple",
+    price: "10BTB",
+  },
+  {
+    id: 2,
+    name: "banana",
+    price: "10BTB",
+  },
+  {
+    id: 3,
+    name: "orange",
+    price: "15BTB",
   },
 ]);
 </script>
-
-<style scoped>
-</style>
