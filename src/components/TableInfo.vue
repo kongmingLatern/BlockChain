f89ca06f81a9adbafb4f0210e1a3ceeb08b54e1d
凤之兮原 <1577310596@qq.com>
2022-09-02 21:25:32 +0800

    feat: add user

--- src/components/TableInfo.vue
+++ src/components/TableInfo.vue
@@ -2,9 +2,13 @@
   <div border border-collapse text-center h-37 w-40 lh-7>
     <header border-b-solid>用户：{{ userInfo.name }}</header>
     <main border-b-solid>
-      <div>出资：<span font-semibold color-red>{{ userInfo.price }}</span></div>
+      <div>
+        出资：<span font-semibold color-red>{{ userInfo.price }}</span>
+      </div>
       <div>年龄：{{ userInfo.age }}</div>
-      <div>余额：<span color-blue>{{ userInfo.wallet }}</span></div>
+      <div>
+        余额：<span color-blue>{{ userInfo.wallet }}</span>
+      </div>
     </main>
     <footer>
       <router-link to="" decoration-none>充值</router-link>
@@ -12,14 +16,15 @@
   </div>
 </template>
 
-<script setup lang='ts'>
-const userInfo = {
-  name: '小明',
+<script setup lang="ts">
+import type { UserInfoType } from "@/type";
+const userInfo: UserInfoType = {
+  id: 1,
+  name: "小明",
   age: 20,
-  wallet: '100BTB',
-  price: '10BTB'
-}
+  wallet: "1000",
+  price: "100",
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
