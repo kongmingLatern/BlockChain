2f8e43d2fcad06297ef4c7b01bcf25f07c43b429
凤之兮原 <1577310596@qq.com>
2022-09-03 22:07:05 +0800

    chore: add title to Provider

--- src/components/ProviderTable.vue
+++ src/components/ProviderTable.vue
@@ -1,6 +1,9 @@
 <template>
   <table border text-center m-0 m-auto w-100>
-    <caption text-left caption-top text-5 color-orange>{{ seller }}</caption>
+    <caption caption-top text-5 color-blue>
+      <span float-left>{{ seller }}</span>
+      <span float-right text-4 color-red>￥{{ sales }} / BTB</span>
+    </caption>
     <tr>
       <th>商品名称</th>
       <th>最低售价</th>
@@ -15,6 +18,7 @@ import type { GoodsItem } from "@/type";
 import GoodsItems from "./GoodsItems.vue";
 defineProps<{
   seller: string;
+  sales: number,
   goodsItem: GoodsItem[]
 }>();
 </script>
