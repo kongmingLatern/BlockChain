b574c5930018e9aeae6f8c63eb1dfc3d3f288a3c
凤之兮原 <1577310596@qq.com>
2022-09-05 21:58:00 +0800

    chore: add Header.vue

---
+++ src/components/index/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header flex justify-between items-center p-2 bg-red h-20>
+    <h1 class="h_100" lh-20 color-white font-semibold>Minimum BlockChain</h1>
+    <Modal />
+  </header>
+</template>
+
+<script setup lang='ts'>
+import Modal from './Modal.vue'
+</script>
+
+<style scoped>
+.h_100 {
+  height: 100%;
+}
+</style>
