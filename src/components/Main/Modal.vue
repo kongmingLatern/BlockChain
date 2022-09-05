3d8ef4f5d68a21df45bad8165808a37a15df2fa4
凤之兮原 <1577310596@qq.com>
2022-09-05 21:21:00 +0800

    feat; add Modal

---
+++ src/components/Main/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <a-button @click="showModal">登录</a-button>
+    <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
+      <Form />
+    </a-modal>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Form from './Form.vue';
+export default defineComponent({
+  setup() {
+    const visible = ref<boolean>(false);
+    const showModal = () => {
+      visible.value = true;
+    };
+    const handleOk = (e: MouseEvent) => {
+      console.log(e);
+      visible.value = false;
+    };
+    return {
+      visible,
+      showModal,
+      handleOk,
+    };
+  },
+  components: { Form }
+});
+</script>
+
