0b00f8d1990449e40cb2f44c50a601b91a747120
凤之兮原 <1577310596@qq.com>
2022-09-05 21:57:51 +0800

    change dir

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
-import Main from "@/views/Main.vue";
-import Modal from '@/components/Main/Modal.vue';
 </script>
 
 <template>
-  <header flex justify-between items-center p-2 bg-red h-20>
-    <h1 class="h_100" lh-20 color-white font-semibold>Minimum BlockChain</h1>
-    <Modal />
-  </header>
-  <Main></Main>
-  <!-- <HomeView /> -->
+  <router-view />
 </template>
 
 <style>
@@ -17,9 +10,5 @@ import Modal from '@/components/Main/Modal.vue';
   padding: 0;
   margin: 0;
   box-sizing: border-box;
-}
-
-.h_100 {
-  height: 100%;
 }
 </style>
